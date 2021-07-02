8d9203d06e2fca600a74661ba454f6c6b84ec45c
Marvelles Fisher <mfisher91@live.com>
2021-07-02 12:11:15 -0700

    second lab

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+let App = () => {
+    return ( <
+        div >
+        <
+        h1 > Hello from App Component! < /h1> <
+        /div>
+    );
+};
+
+export default App;
